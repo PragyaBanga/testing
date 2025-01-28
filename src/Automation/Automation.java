4bff6b487e0a9bea681ed5a73d96d83644bb4873
PragyaBanga <Pragya.b0501@gmail.com>
2025-01-28 16:10:55 +0530

    second commit

--- src/Automation/Automation.java
+++ src/Automation/Automation.java
@@ -1,5 +1,11 @@
 package Automation;
 
 public class Automation {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+		System.out.println("my first automation class");
+	}
 
 }
